effbaa1e8bdaae7dbe5bc643982895faf9a83d82
SUPREMEchelson <chelson.sm@gmail.com>
2021-12-30 03:31:15 +0100

    Forum

---
+++ TDComposite/src/main/java/Forum/Message.java
@@ -0,0 +1,14 @@
+package Forum;
+
+public class Message {
+    private final String top;
+    private final String contents;
+
+    public Message(String top,String contents){
+        this.top = top;
+        this.contents = contents;
+    }
+    public  String subjet(){
+        return this.top;
+    }
+}
